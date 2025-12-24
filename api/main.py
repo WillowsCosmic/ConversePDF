7da3e2cf078c46f6816122b3a1bf732599f6c6ee
WillowsCosmic <purbalitech8@gmail.com>
2025-12-24 15:05:46 +0530

    New Changes

--- api/main.py
+++ api/main.py
@@ -5,7 +5,7 @@ import logging
 from fastapi import FastAPI
 import inngest
 import inngest.fast_api
-from inngest.experimental import ai
+#from inngest.experimental import ai
 from dotenv import load_dotenv
 import uuid
 import os
